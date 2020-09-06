595d2e9310cd2b3493627dc02304523c549f9e2b
Bertrand Roussel <broussel@sierrawireless.com>
2020-09-06 08:24:16 -0700

    Add DEBUG_GERRIT mode
    Change-Id: I831f3663a8667ca723470e840a892b248ac86257

--- gerrit-start.sh
+++ gerrit-start.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env sh
 
-set -e
-
 echo "Starting Gerrit..."
 exec su-exec ${GERRIT_USER} ${GERRIT_SITE}/bin/gerrit.sh ${GERRIT_START_ACTION:-daemon}
 RET=$?
 
 echo "Exit $RET"
+
+if [ -n "$DEBUG_GERRIT" ]; then
+    echo "Debug Gerrit mode, staying alive"
+    tail -f /dev/null
+fi
+
 exit $RET
 
